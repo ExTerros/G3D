fdf5a5ee198739b8bf06cf7df43019412cdef88a
exterros <nicolasdu76250@live.fr>
2023-05-07 01:00:00 +0200

    ouais ouais ouais

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 window.onload = () => {
+
+    // Définir les variables
+    let images = ["assets/slider/deratisation-01.jpg", "assets/slider/deratisation-02.jpg", "assets/slider/fumigation-01.jpg"];
+    let currentImage = 0;
+    let imageContainer = document.querySelector('#image-slider');
+
+    // Changer l'image toutes les 8 secondes
+    setInterval(() => {
+        currentImage = (currentImage + 1) % images.length;
+        imageContainer.classList.add('fade-out');
+        setTimeout(() => {
+            imageContainer.src = images[currentImage];
+            imageContainer.classList.remove('fade-out');
+        }, 500);
+    }, 8000);
+
     var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
     var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
 
@@ -11,7 +27,7 @@ window.onload = () => {
 
     var themeToggleBtn = document.getElementById('theme-toggle');
 
-    themeToggleBtn.addEventListener('click', function() {
+    themeToggleBtn.addEventListener('click', function () {
 
         // toggle icons inside button
         themeToggleDarkIcon.classList.toggle('hidden');
@@ -27,7 +43,7 @@ window.onload = () => {
                 localStorage.setItem('color-theme', 'light');
             }
 
-        // if NOT set via local storage previously
+            // if NOT set via local storage previously
         } else {
             if (document.documentElement.classList.contains('dark')) {
                 document.documentElement.classList.remove('dark');
@@ -37,6 +53,81 @@ window.onload = () => {
                 localStorage.setItem('color-theme', 'dark');
             }
         }
-        
+
+    });
+
+    gsap.registerPlugin(ScrollTrigger);
+
+    const navText = document.querySelectorAll('#nav-ul');
+    gsap.set(navText, { opacity: 0, y: -50 });
+    gsap.to(navText, { duration: 1, opacity: 1, y: 0, stagger: 0.2 });
+
+    const elements = document.querySelectorAll('#TextAChanger');
+    gsap.set(elements, { opacity: 0, x: -100 });
+    gsap.to(elements, { duration: 1, opacity: 1, x: 0, stagger: 0.2 });
+
+    const fumigation = document.querySelectorAll("#fumigation");
+    const dératisation = document.querySelectorAll("#dératisation");
+
+    gsap.set(fumigation, { opacity: 0, x: 300 });
+    gsap.set(dératisation, { opacity: 0, x: 300 });
+
+    gsap.to(fumigation, {
+        duration: 1,
+        opacity: 1,
+        x: 0,
+        stagger: 0.2,
+        scrollTrigger: {
+            trigger: fumigation,
+            start: "top 80%",
+            end: "bottom 20%",
+            toggleActions: "play none none reverse",
+        },
+    });
+
+    gsap.to(dératisation, {
+        duration: 1,
+        opacity: 1,
+        x: 0,
+        stagger: 0.2,
+        scrollTrigger: {
+            trigger: dératisation,
+            start: "top 80%",
+            end: "bottom 20%",
+            toggleActions: "play none none reverse",
+        },
+    });
+
+    const désinsectisation = document.querySelectorAll("#désinsectisation");
+    const désinfection = document.querySelectorAll("#désinfection");
+
+    gsap.set(désinsectisation, { opacity: 0, x: -300 });
+    gsap.set(désinfection, { opacity: 0, x: -300 });
+
+    gsap.to(désinsectisation, {
+        duration: 1,
+        opacity: 1,
+        x: 0,
+        stagger: 0.2,
+        scrollTrigger: {
+            trigger: désinsectisation,
+            start: "top 80%",
+            end: "bottom 20%",
+            toggleActions: "play none none reverse",
+        },
+    });
+
+    gsap.to(désinfection, {
+        duration: 1,
+        opacity: 1,
+        x: 0,
+        stagger: 0.2,
+        scrollTrigger: {
+            trigger: désinfection,
+            start: "top 80%",
+            end: "bottom 20%",
+            toggleActions: "play none none reverse",
+        },
     });
 }
+
